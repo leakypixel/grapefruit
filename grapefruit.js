fde93aa19ecfb34209cf8313a22b31c75af62aa7
Craig Fletcher <hi@leakypixel.net>
2019-11-06 13:56:56 +0000

    Move to constructors, add history as emitter

--- grapefruit.js
+++ grapefruit.js
@@ -1,89 +1,154 @@
-const Grapefruit = config => {
-  let history = [];
-  const funcs = config.funcs;
-  const curryFunc = (config, func) => {
-    return item => func(config, item);
-  };
+const applyConfigToFunc = (config, func) => {
+  return item => func(config, item);
+};
 
-  const runStep = (state, step) => {
+function Grapefruit(config) {
+  const _self = this;
+  this.funcs = config.funcs;
+
+  this.emitter = config.emitter ? config.emitter : () => null;
+
+  this.runStep = (state, step) => {
     return new Promise((resolve, reject) => {
       const selectors = {
         selectAll: () => state,
         selectMany: filter => state.filter(filter),
         selectByTag: tag =>
           state.filter(item => item.tags && item.tags.includes(tag)),
-        selectOne: filter => state.find(filter)
+        selectOne: filter => state.find(filter) || []
       };
 
       Promise.all(
-        step.reduce((actions, func) => {
-          if (!funcs[func.name]) {
-            throw new Error(`Named function does not exist: ${func.name}`);
+        step.reduce((actions, action) => {
+          const id = Math.round(Math.random() * 1000000);
+
+          const actionFunction = _self.funcs[action.func];
+          if (!actionFunction) {
+            reject({
+              message: `Named function ${
+                action.func
+              } does not exist on action ${action.name ? action.name : id}.`,
+              action: action,
+              id
+            });
           }
 
-          const stepConfig = Object.assign(
+          const actionConfig = Object.assign(
             {},
-            func.config,
-            func.getConfig && func.getConfig(selectors)
+            action.config,
+            action.getConfig && action.getConfig(selectors)
           );
-          const curriedFunc = curryFunc(stepConfig, funcs[func.name]);
+          const actionWithConfig = applyConfigToFunc(
+            actionConfig,
+            actionFunction
+          );
+          _self.emitter({
+            eventType: "actionConfigured",
+            action,
+            actionConfig,
+            id
+          });
 
-          if (func.selector) {
-            const selectedState = [].concat(func.selector(selectors));
-            return actions.concat(selectedState.map(item => curriedFunc(item)));
-          } else {
-            console.log(
-              `Function ${
-                func.name
-              } has no selector, running as single instance.`
-            );
-            return actions.concat(curriedFunc(func.item));
-          }
+          const selectedState = [].concat(
+            action.selector ? action.selector(selectors) : action.item
+          );
+          _self.emitter({
+            eventType: "stateSelected",
+            action,
+            selectedState,
+            id
+          });
+
+          return actions.concat(
+            selectedState.map(item => {
+              return new Promise((resolve, reject) => {
+                _self.emitter({
+                  eventType: "actionRunning",
+                  action,
+                  item,
+                  actionConfig,
+                  id
+                });
+                const actionRunner = actionWithConfig(item);
+                actionRunner
+                  .then(result => {
+                    _self.emitter({
+                      eventType: "actionComplete",
+                      action,
+                      item,
+                      result,
+                      id
+                    });
+                    resolve(result);
+                  })
+                  .catch(e => {
+                    const error = {
+                      action,
+                      item,
+                      id,
+                      ...e
+                    };
+                    _self.emitter(error);
+                    reject(error);
+                  });
+              });
+            })
+          );
         }, [])
       )
         .then(result => {
-          console.log("Result:", result);
+          _self.emitter({ eventType: "stepActionsComplete", step, result });
           resolve(result.flat());
         })
-        .catch(e => reject(e));
+        .catch(e => {
+          reject(e);
+        });
     });
   };
 
-  const runPipeline = pipeline => {
-    return new Promise(resolve => {
+  this.runPipeline = pipeline => {
+    return new Promise((resolve, reject) => {
       const doStep = (state, steps) => {
-        if (config.keepHistory) {
-          history.push(state);
-        }
+        _self.emitter({ eventType: "newState", state: state });
 
-        const step = runStep(state, steps[0]);
+        const step = this.runStep(state, steps[0]);
         step
           .then(newState => {
+            _self.emitter({
+              eventType: "stepComplete",
+              step: steps[0],
+              result: newState
+            });
             if (steps.length > 1) {
               doStep(newState, steps.slice(1));
             } else {
-              if (config.keepHistory) {
-                history.push(newState);
-              }
+              _self.emitter({ eventType: "newState", state: newState });
+              _self.emitter({ eventType: "pipelineComplete", state: newState });
               resolve(newState);
             }
           })
-          .catch(e => console.log("Error caught:", e));
+          .catch(e => {
+            const error = { eventType: "error", ...e, step: steps[0] };
+            _self.emitter(error);
+            reject(error);
+          });
       };
 
-      if (pipeline.getInitialState) {
-        doStep(pipeline.getInitialState(pipeline), pipeline.steps);
-      } else {
-        doStep(pipeline.initialState || [], pipeline.steps);
-      }
+      const initialState = Object.assign(
+        [],
+        pipeline.initialState,
+        pipeline.getInitialState && pipeline.getInitialState(selectors)
+      );
+      doStep(initialState, pipeline.steps);
     });
   };
+}
 
-  return {
-    config,
-    runPipeline,
-    history
-  };
+Grapefruit.History = function History() {
+  let timeline = [];
+  this.get = () => timeline;
+  this.push = item => timeline.push(item);
+  this.flush = () => (timeline = []);
 };
 
 module.exports = Grapefruit;
